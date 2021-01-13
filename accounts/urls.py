da087305dd3a55e50fb78a333666b3d86300d1e0
Tripti Sharma <triptisharmably@gmail.com>
2021-01-13 05:13:11 -0500

    Added project from 0

---
+++ accounts/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views 
+
+urlpatterns = [
+    path('register/',views.registerPage, name="register"),
+    path('login/',views.loginPage, name="login"),
+    path('logout/',views.logoutUser, name="logout"),
+
+    path('',views.home, name="home"),
+
+    path('account/', views.accountSettings, name="account"),
+    path('user/',views.userPage, name="user-page"),
+    path('products/',views.products, name='products'),
+    path('customer/<str:pk_test>/',views.customers, name="customer"),
+    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
+]
